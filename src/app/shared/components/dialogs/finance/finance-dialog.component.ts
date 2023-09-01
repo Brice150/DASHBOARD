f24f82d7d0bcedc777b42b19287bf98fef110a74
brice150 <brice.lecomte0@gmail.com>
2023-09-01 22:43:22 +0200

    first graphs

--- src/app/shared/components/dialogs/finance/finance-dialog.component.ts
+++ src/app/shared/components/dialogs/finance/finance-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import Chart from 'chart.js/auto';
 import { FinanceInfos } from 'src/app/core/interface/financeInfos';
 
 @Component({
@@ -19,6 +20,36 @@ export class FinanceDialogComponent implements OnInit {
   ngOnInit() {
     this.financeInfo = this.data.financeInfo;
     this.index = this.data.index;
+
+    const graph = document.getElementById('financeGraph') as HTMLCanvasElement | null;
+    if (graph) {
+      const financeGraph = new Chart(graph, {
+        type: 'bar',
+        data: {
+          labels: ['1 Year', '2 Year', '3 Year'],
+          datasets: [
+            {
+              label: 'Invested',
+              data: [1, 2, 3]
+            },
+            {
+              label: 'Interests',
+              data: [0.5, 3, 5]
+            }
+          ]
+        },
+        options: {
+          scales: {
+            x: {
+              stacked: true
+            },
+            y: {
+              stacked: true
+            }
+          }
+        }
+      });
+    }
   }
 
   close() {
--- src/app/shared/components/dialogs/weather/weather-dialog.component.ts
+++ src/app/shared/components/dialogs/weather/weather-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Chart } from 'chart.js/auto';
 import { WeatherInfos } from 'src/app/core/interface/weatherInfos';
 
 @Component({
@@ -16,9 +17,33 @@ export class WeatherDialogComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
 
-  ngOnInit() {
+  ngOnInit() {    
     this.weatherInfo = this.data.weatherInfo;
     this.index = this.data.index;
+
+    const graph = document.getElementById('weatherGraph') as HTMLCanvasElement | null;
+    if (graph) {
+      const weatherGraph = new Chart(graph, {
+        type: 'line',
+        data: {
+          labels: ['11h', '12h', '13h'],
+          datasets: [
+            {
+              label: 'Precipitation',
+              data: [1, 0, 0]
+            },
+            {
+              label: 'Temperature',
+              data: [12, 19, 3]
+            },
+            {
+              label: 'Windspeed',
+              data: [10, 15.5, 2]
+            },
+          ]
+        }
+      });
+    }
   }
 
   close() {
