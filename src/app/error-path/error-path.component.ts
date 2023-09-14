3da3caad6663eee63c21e9532afb6157353025f5
brice150 <brice.lecomte0@gmail.com>
2023-09-14 13:35:40 +0200

    background color change with user preference

--- src/app/error-path/error-path.component.ts
+++ src/app/error-path/error-path.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import {
+  FinanceInfos,
+  MoneyInput,
+  Yearly,
+} from '../core/interfaces/financeInfos';
+import { User } from '../core/interfaces/user';
 
 @Component({
   selector: 'app-error-path',
@@ -7,11 +13,89 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./error-path.component.css'],
 })
 export class ErrorPathComponent implements OnInit {
+  user: User = {} as User;
+  defaultCityName: string = 'Paris';
+
   constructor(private toastr: ToastrService) {}
 
   ngOnInit() {
+    let storedUser: string | null = localStorage.getItem('userDashboard');
+    if (storedUser !== null) {
+      this.user = JSON.parse(storedUser);
+    } else {
+      this.generateDefaultUser();
+      localStorage.setItem('userDashboard', JSON.stringify(this.user));
+    }
+    this.handleMode();
+
     this.toastr.error('URL not available', 'URL', {
       positionClass: 'toast-bottom-center',
     });
+  }
+
+  generateDefaultUser() {
+    const moneyInput: MoneyInput = {
+      amountPerMonth: 100,
+      initialAmount: 1000,
+      percentage: 8,
+    };
+
+    const yearly: Yearly = {
+      date: [
+        '0',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+        '11',
+        '12',
+        '13',
+        '14',
+        '15',
+        '16',
+        '17',
+        '18',
+        '19',
+        '20',
+        '21',
+        '22',
+        '23',
+        '24',
+        '25',
+      ],
+      invested: [moneyInput.initialAmount],
+      interests: [0],
+      total: [moneyInput.initialAmount],
+    };
+
+    const financeInfos: FinanceInfos = {
+      moneyInput: moneyInput,
+      yearly: yearly,
+    };
+
+    this.user = {
+      prefersDarkMode: false,
+      city: this.defaultCityName,
+      financeInfos: financeInfos,
+      tasks: [],
+    };
+  }
+
+  handleMode() {
+    if (this.user.prefersDarkMode) {
+      document.body.classList.add('dark-theme-variables');
+    } else {
+      document.body.classList.remove('dark-theme-variables');
+    }
+    this.user.prefersDarkMode = document.body.classList.contains(
+      'dark-theme-variables'
+    );
+    localStorage.setItem('userDashboard', JSON.stringify(this.user));
   }
 }
