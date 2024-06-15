a3b3074c814536a27f45a793818f9d0d10351e98
brice150 <brice.lecomte0@gmail.com>
2024-06-15 11:41:14 +0200

    better graph colors + real estate simulator start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,16 @@
 import { Routes } from '@angular/router';
 import { PageComponent } from './page/page.component';
+import { WeatherCityComponent } from './page/weather/weather-city/weather-city.component';
+import { FinanceRealEstateComponent } from './page/finance/finance-real-estate/finance-real-estate.component';
+import { FinanceStockExchangeComponent } from './page/finance/finance-stock-exchange/finance-stock-exchange.component';
+import { FinanceIncomeSavingsComponent } from './page/finance/finance-income-savings/finance-income-savings.component';
 
 export const routes: Routes = [
   { path: '', component: PageComponent },
+  { path: 'income', component: FinanceIncomeSavingsComponent },
+  { path: 'savings', component: FinanceIncomeSavingsComponent },
+  { path: 'stock-exchange', component: FinanceStockExchangeComponent },
+  { path: 'real-estate', component: FinanceRealEstateComponent },
+  { path: 'weather/:city', component: WeatherCityComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
